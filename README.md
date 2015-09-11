06a1e3873351e08eab26559cb3f5513e11dd7bde
Jay Jakosky <jay_jakosky@trimble.com>
2015-09-11 16:01:49 -0600

    Initial release.

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # aws-snapshot-tender
 A better way to create and prune snapshots in AWS EC2.
+
+## Introduction
+EC2 Snapshots are an endless stream. Unlike real tapes, they don't get lost. The only things we need to configure are how often to take snapshots (recurrence) and which snapshots to keep as they age (retention).
+
+We don't need to consider the type of snapshot--incremental versus full--because there's only one: incremental changed blocks. The only incentive to pruning is to cut snapshot storage costs. Otherwise we could take an infinite number of snapshots in the endless steam.
+
+This tool cleanly separates the functions of creating and pruning.
+
+### How it works:
+
+ * Searches all volumes and instances in a region for a *Snapshot* tag.
+ * *Snapshot* tags contain recurrence and retention policies.
+ * A volume will inherit its policy from the attached instance, but this can be overridden on the volume.
+ * *Recurrence*: How often is a snapshot required? If we don't have a recent one, create it.
+ * *Retention*: Looking back in time, which snapshots should I keep?
+
+### Advantages:
+
+ * Retention is not driven by how frequently this script is run. Retention is written into the *Snapshot* tag.
+ * Running this script more frequently means you will respond more quickly to the need for a new snapshot. It will not create more and more snapshots, and it will not delete too many snapshots.
+ * All snapshots for a volume will be included in the retention calculation. So feel free to take additional snapshots at any time, such as just before a dangerous change or after an important update.
+ * If you want your snapshot retention windows to be calculated from a strict boundary like 00:00 UTC, you can supply your own time.
+ * If you accidentally run two copies of this script from different locations WITH ACCURATE CLOCKS, you won't mess up the results. The scripts will calculate the same. It's the same as running this script more frequently. If the clocks are significantly different, or one script is given a custom time parameter, then that may over-prune.
+
+
+## Recurrence and Retention
+
+Recurrence is simply "how often a snapshot is required". **aws-snapshot-tender** looks at the recurrence on each volume and instance. If enough time has passed, **aws-snapshot-tender** makes a request for a snapshot.
+
+Retention can be visualized as a series of windows stretching back in time, starting from right now. The first window back in time might be for a day, then another window for a month. When these windows are laid over the timeline of snapshots, we see that some windows have more than one snapshot. Only the most recent snapshot in each window is kept. So to keep more snapshots, make the windows smaller.
+
+Snapshot tags have just one element to define recurrence (starting with `@`) and additional elements for retention (starting with `+`)
+
+Here's an example:
+```script
+@4h +4h6 +1d7 +1w4 +1m12
+```
+
+* Take a snapshot @ 4 hour intervals.
+* Retain a snapshot every 4 hour interval, for 6 intervals.
+* After that... Retain a snapshot every 1 day, for 7 days.
+* After that... Every week for 4 weeks.
+* After that... Every month for 12 months.
+* Total retention time would be: 12 months + 4 weeks + 7 days + 24 hours. = 13.25 months.
+
+### Ignore
+To ignore volumes of a configured instance, or to temporarily ignore an instance, start the tag value with a hyphen.
+* ``` ---DO NOT BACKUP--- ```
+
+* ```  --   @2h +1h24 +1d7 +1w4 +1m36 ```
+
+
+### Notes & Clarifications
+* Tags with a recurrence but no retention will create snapshots that are never pruned.
+* A volume with a defined snapshot tag will take precedence over an instance set to ignore.
+* Be sure the recurrence is small enough. A retention of 1 day doesn't make sense with a recurrence of 1 week, because 6 one-day windows would be skipped waiting for another snapshot to be created.
+
+## Requirements
+**aws-snapshot-tender** was developed with the following versions.
+* Python 2.7.9
+* Boto 2.38.0
+* ConfigArgParse 0.93
+
+## Usage
+```
+usage: aws-snapshot-tender.py [-h] (--create | --prune) [--tag TAG]
+                              [--time TIME] [-c CONFIG] -r REGIONS
+                              [REGIONS ...] [--dry-run] [--profile PROFILE]
+                              [--awsid AWSID] [--awskey AWSKEY]
+                              [--logfile LOGFILE]
+
+Args that start with '--' (eg. --create) can also be set in a config file
+(/Users/jay/trimble/devops/operations/aws-snapshot-tender/aws-snapshot-
+tender.py.conf or specified via -c) by using .ini or .yaml-style syntax (eg.
+create=value). If an arg is specified in more than one place, then command-
+line values override config file values which override defaults.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --create              Create snapshots.
+  --prune               Prune snapshots.
+  --tag TAG             Tag to search for.
+  --time TIME           Timestamp to use for comparison.
+  -c CONFIG, --config CONFIG
+                        Config file.
+  -r REGIONS [REGIONS ...], --regions REGIONS [REGIONS ...]
+                        Regions to connect to.
+  --dry-run             Do not make changes.
+  --profile PROFILE     Boto profile name.
+  --awsid AWSID         AWS Access Key ID
+  --awskey AWSKEY       AWS Secret Access Key
+  --logfile LOGFILE     Logfile location. Default is current directory.
+```
---
+++ aws-snapshot-tender.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+
+'''
+Copyright (c) 2015 Jay Jakosky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import os
+import boto.ec2
+import configargparse
+import logging
+from datetime import datetime, timedelta
+import re
+from collections import defaultdict
+
+superstructure = []
+
+def ec2_data_into_superstructure(region, credentials, tagname):
+  ''' Fetch volumes, instances and snapshots from EC2.
+      For each volume, instantiate a combined volume-instance-snapshot structure.
+      Add this to a global list.
+  '''
+  global superstructure
+  conn = boto.ec2.connect_to_region(region, **credentials)
+  snapshots = conn.get_all_snapshots(owner='self')
+  logging.info("Snapshots: Fetched %s snapshots." % len(snapshots))
+  volumes = conn.get_all_volumes()
+  logging.info("Volumes: Fetched %s volumes." % len(volumes))
+  instances = conn.get_only_instances()
+  logging.info("Instances: Fetched %s instances." % len(instances))
+
+  instancedict = {instance.id : instance for instance in instances}
+  snapshotdict = defaultdict(list)
+  for snapshot in snapshots:
+    snapshotdict[snapshot.volume_id].append(snapshot)
+
+  for vol in volumes:
+    instance = None
+    try:
+      attachment_state = vol.attachment_state() if vol.attachment_state() else ''
+      if attachment_state[:6] == 'attach':
+        instance_id = vol.attach_data.instance_id
+        instance = instancedict[instance_id]
+    except: pass
+    snaps = None
+    try:
+      snaps = snapshotdict[vol.id] 
+    except: pass
+    superstructure.append(VolInstSnapStruct(vol, instance, snaps, tagname))
+
+class VolInstSnapStruct:
+  ''' This class represents a single volume, plus a possible instance to which it is attached, plus all of the volume's snapshots.
+  '''
+  def __init__(self, volume, instance, snapshots, tagname):
+    self.instance_id = None
+    self.instance_name = None
+    self.tag = None
+    self.tagsource = None
+    self.recurrence = None
+    self.retention = None
+
+    self.volume = volume
+    self.instance = instance
+    self.snapshots = snapshots
+
+    self.volume_id = self.volume.id
+
+    try:
+      self.instance_id = self.instance.id
+    except: pass
+
+    try:
+      self.instance_name = self.instance.tags['Name']
+    except: pass
+
+    try:
+      self.tag = (self.volume).tags[tagname]
+      self.tagsource = self.volume_id
+    except:
+      try:
+        self.tag = (self.instance).tags[tagname]
+        self.tagsource = self.instance_id
+      except:
+        pass
+
+    if self.find_recurrence(self.tag):
+      self.recurrence = self.find_recurrence(self.tag)
+      logging.info("%s: found recurrence '%s' from %s" %(self.volume_id, self.tag, self.tagsource))
+    if self.find_retention(self.tag):
+      self.retention = self.find_retention(self.tag)
+      logging.info("%s: found retention '%s' from %s" %(self.volume_id, self.tag, self.tagsource))
+
+  def has_recurrence(self):
+    return True if self.recurrence else False
+
+  def has_retention(self):
+    return True if self.retention else False
+
+  def is_eligible(self):
+    is_eligible = True
+    try:
+      if self.tag[0] == '-':
+        is_eligible = False
+    except:
+      pass
+    return is_eligible
+
+  def find_recurrence(self,tag):
+    ''' Returns: count, period
+    '''
+    try:
+      match = re.findall('^\s*\@([1-9]+[0-9]*)([hdwm])',tag)
+    except:
+      return None
+    if len(match)==1:
+      return match[0]
+    else:
+      return None
+
+  def find_retention(self,tag):
+    ''' Returns: count, period, repeat
+    '''
+    try:
+      matches = re.findall('\+([1-9]+[0-9]*)([hdwm])([1-9]*[0-9]*)',tag)
+    except:
+      return None
+    if len(matches)>0:
+      return matches
+    else:
+      return None
+
+  def recent_snapshots(self,cutoff):
+    ''' Search snapshot dictionary for the volume id, and check for any snapshots that are more recent than the cutoff date.
+    '''
+    recent_snapshots = []
+    for snapshot in self.snapshots:
+      start_time = datetime.strptime(snapshot.start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+      if start_time > cutoff: #if snapshot is more recent than cutoff
+        recent_snapshots.append(snapshot)
+    return recent_snapshots if len(recent_snapshots)>0 else None
+
+  def create_snapshot(self, description):
+    self.volume.create_snapshot(description)
+
+  def match_snapshots_to_windows(self,windows):
+    ''' Given a list of time windows, match the snapshots to any applicable time windows.
+    '''
+    matched_windows = defaultdict(list)
+    for snapshot in self.snapshots:
+      start_time = datetime.strptime(snapshot.start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+      for recent, distant in windows:
+        if distant < start_time < recent:
+          logging.info("%s: Window (%s, %s) contains %s (%s)." % (snapshot.volume_id, recent, distant, snapshot.id, start_time))
+          matched_windows[(recent, distant)].append(snapshot)
+    return matched_windows
+  ## End class.
+
+
+def cutoff_dt(dt, period, count):
+  count = int(count)
+  mcount = count-1 if count>0 else 0
+  compare = {
+          "h": dt-timedelta(hours=int(count)),
+          "d": dt-timedelta(days= int(count)),
+          "w": dt-timedelta(days= 7 * int(count)),
+          "m": datetime(dt.year-int(mcount)//12, dt.month-int(mcount)%12, 1)-timedelta(milliseconds=1)
+      }
+  return compare.get(period, None)
+
+def convert_to_windows(time, retention):
+  windows = []
+  recent = time
+  for count, period, repeat in retention:
+    repeat = int(repeat) if len(repeat)>0 else 1
+    for i in range(1, repeat+1):
+      distant = cutoff_dt(recent, period, 1)
+      windows.append((recent, distant))
+      recent = distant
+  return windows
+
+def create_snapshots(time, dry_run):
+  ''' Find volumes that require a new snapshot.
+      Recurrence timing is defined in the first element of a volume tag.
+      Compare the age of the existing snapshots with the recurrence timing.
+  ''' 
+
+  logging.info("Timestamp for comparison: %s" % time)
+  create_requests = []
+  logging.info("Searching for volumes to snapshot...")
+  for v in superstructure:
+    recurrence_found = False
+    count = None
+    period = None
+    if v.is_eligible():
+      if v.has_recurrence():
+        logging.info("%s: Has recurrence '%s'." % (v.volume_id,v.tag))
+        count,period = v.recurrence
+        cutoff = cutoff_dt(time, period, count)
+        if v.recent_snapshots(cutoff):
+          logging.info("%s has recent snapshot %s prior to cutoff %s." % (v.volume_id,[str(snap.id) for snap in v.recent_snapshots(cutoff)],cutoff))
+        else:
+          logging.info("Snapshot required for %s." % v.volume_id)
+          create_requests.append(v)
+    else:
+      logging.info("%s is not eligible." % v.volume_id)
+  logging.info("Search complete.")
+  if dry_run:
+    logging.info("Dry run. No create requests.")
+  else:
+    for v in create_requests:
+      logging.info("Creating snapshot for %s from %s" % (v.volume_id, v.instance_id))
+      v.create_snapshot("%s %s" % (v.volume_id, v.instance_id))
+
+
+def prune_snapshots(time, dry_run):
+  delete_requests = []
+  logging.info("Searching for snapshots to prune...")
+  for v in superstructure:
+    windows = None
+    if v.has_retention():
+      logging.info("%s: Has retention '%s'." % (v.volume_id,v.tag))
+      windows = convert_to_windows(time, v.retention)
+      logging.info("%s: Matching snapshots to windows." % v.volume_id)
+      matched_windows = v.match_snapshots_to_windows(windows)
+      for key,snaps in matched_windows.iteritems():
+        snaps.sort(key=lambda snap: datetime.strptime(snap.start_time, '%Y-%m-%dT%H:%M:%S.%fZ'))
+        try:
+          snaps.pop() # Remove highest-date snapshot, which is the most recent in the window.
+        except:
+          pass
+        if len(snaps)>0:
+          for snap in snaps:
+            logging.info("%s: Pruning eligible for %s from %s '%s'" % (v.volume_id, snap.id, v.instance_id, v.instance_name))
+            delete_requests.append(snap)
+  logging.info("Search complete.")
+  if dry_run:
+    logging.info("Dry run requested. No delete requests.")
+  else:
+    for snapshot in delete_requests:
+      logging.info("Deleting %s" % snapshot.id)
+      try:
+        snapshot.delete()
+      except: pass
+
+
+if __name__ == "__main__":
+  p = configargparse.ArgParser(
+    default_config_files=[os.path.abspath(__file__)+'.conf'])
+  group = p.add_mutually_exclusive_group(required=True)
+  group.add_argument('--create', action="store_true", help="Create snapshots.")
+  group.add_argument('--prune', action="store_true", help="Prune snapshots.")
+  p.add('--tag', required=False, help='Tag to search for.', default='Snapshot')
+  p.add('--time', required=False, help='Timestamp to use for comparison.', default=datetime.utcnow())
+  p.add('-c', '--config', required=False, help='Config file.',
+    is_config_file=True)
+  p.add('-r', '--regions', required=True, nargs='+', help='Regions to connect to.')
+  p.add('--dry-run', required=False, action="store_true", help='Do not make changes.')
+  p.add('--profile', required=False, help='Boto profile name.')
+  p.add('--awsid', required=False, help='AWS Access Key ID')
+  p.add('--awskey', required=False, help='AWS Secret Access Key')
+  p.add('--logfile', required=False, help='Logfile location. Default is current directory.',
+                     default=os.path.abspath(__file__)+'.log')
+  args = p.parse_args()
+
+  logging.basicConfig(level=logging.DEBUG,
+                      format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                      datefmt='%y-%m-%d %H:%M',
+                      filename=args.logfile,
+                      filemode='w')
+  console = logging.StreamHandler()
+  console.setLevel(logging.INFO)
+  formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+  console.setFormatter(formatter)
+  logging.getLogger('').addHandler(console)
+
+  credentials = {}
+  if args.awsid:
+    credentials['aws_access_key_id']=args.awsid
+    credentials['aws_secret_access_key']=args.awskey
+  elif args.profile:
+    credentials['profile_name']=args.profile
+
+  for region in args.regions:
+    logging.info("Fetching volumes, instances and snapshots...")
+    ec2_data_into_superstructure(region, credentials, args.tag)
+    if args.create:
+      create_snapshots(args.time, args.dry_run)
+    elif args.prune:
+      prune_snapshots(args.time, args.dry_run)
+
